651667127dbf5a6ffc2634cc9e146ef7c9f061b6
TowerDefense2D <108612331+TowerDefense2D@users.noreply.github.com>
2022-07-03 11:43:38 +0100

    Levels (Level Manager)
    Made By Carlos Alves

---
+++ Level/LevelManager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Script de controle de nível.
+/// </summary>
+public class LevelManager : MonoBehaviour
+{
+	// cena da interface do utilizador. Carregar no início do nível
+	public string levelUiSceneName;
+	// Quantidade de ouro para este nível
+	public int goldAmount = 20;
+	// Quantas vezes os inimigos podem alcançar o ponto de captura antes da derrota
+	public int defeatAttempts = 1;
+	// Lista com inimigos gerados aleatoriamente permitidos para este nível
+	public List<GameObject> allowedEnemies = new List<GameObject>();
+	// Lista com torres permitidas para este nível
+	public List<GameObject> allowedTowers = new List<GameObject>();
+	// Lista com feitiços permitidos para este nível
+	public List<GameObject> allowedSpells = new List<GameObject>();
+
+	// Gerador de interface do utilizador
+	private UiManager uiManager;
+	// Números de spawners inimigos neste nível
+	private int spawnNumbers;
+	// Contador solto atual
+	private int beforeLooseCounter;
+	// Condição de vitória ou derrota já acionada
+	private bool triggered = false;
+
+    /// <summary>
+    /// Ativar esta instância
+    /// </summary>
+    void Awake()
+    {
+		// Carregar cena da interface do utilizador
+		SceneManager.LoadScene(levelUiSceneName, LoadSceneMode.Additive);
+    }
+
+	/// <summary>
+	/// Começar esta instância
+	/// </summary>
+	void Start()
+	{
+		uiManager = FindObjectOfType<UiManager>();
+		SpawnPoint[] spawnPoints = FindObjectsOfType<SpawnPoint>();
+		spawnNumbers = spawnPoints.Length;
+		if (spawnNumbers <= 0)
+		{
+			Debug.LogError("Have no spawners");
+		}
+		// Define uma lista de inimigos aleatórios para cada spawner
+		foreach (SpawnPoint spawnPoint in spawnPoints)
+		{
+			spawnPoint.randomEnemiesList = allowedEnemies;
+		}
+		Debug.Assert(uiManager, "Wrong initial parameters");
+		// Define a quantidade de ouro para este nível
+		uiManager.SetGold(goldAmount);
+		beforeLooseCounter = defeatAttempts;
+		uiManager.SetDefeatAttempts(beforeLooseCounter);
+	}
+
+    /// <summary>
+    /// Ativa o enable event
+    /// </summary>
+    void OnEnable()
+    {
+        EventManager.StartListening("Captured", Captured);
+        EventManager.StartListening("AllEnemiesAreDead", AllEnemiesAreDead);
+    }
+
+    /// <summary>
+    /// Ativa o disable event
+    /// </summary>
+    void OnDisable()
+    {
+        EventManager.StopListening("Captured", Captured);
+        EventManager.StopListening("AllEnemiesAreDead", AllEnemiesAreDead);
+    }
+
+	/// <summary>
+	/// O inimigo atingiu o ponto de captura.
+	/// </summary>
+	/// <param name="obj">Objeto</param>
+	/// <param name="param">Perimetro</param>
+	private void Captured(GameObject obj, string param)
+    {
+		if (beforeLooseCounter > 0)
+		{
+			beforeLooseCounter--;
+			uiManager.SetDefeatAttempts(beforeLooseCounter);
+			if (beforeLooseCounter <= 0)
+			{
+				triggered = true;
+				// Derrota
+				EventManager.TriggerEvent("Defeat", null, null);
+			}
+		}
+    }
+
+	/// <summary>
+	/// Todos os inimigos estão mortos.
+	/// </summary>
+	/// <param name="obj">Objeto</param>
+	/// <param name="param">Perimetro</param>
+	private void AllEnemiesAreDead(GameObject obj, string param)
+    {
+        spawnNumbers--;
+		//Inimigos mortos em todos os spawners
+		if (spawnNumbers <= 0)
+        {
+			// Verifique se a condição solta não foi acionada antes
+			if (triggered == false)
+			{
+				// Vitória
+				EventManager.TriggerEvent("Victory", null, null);
+			}
+        }
+    }
+}
---
+++ Level/WavesInfo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Tempos das rondas pertencentes aos inimigas.
+/// </summary>
+[ExecuteInEditMode]
+public class WavesInfo : MonoBehaviour
+{
+	// Entre as rondas
+	public List<float> wavesTimeouts = new List<float>();
+
+	// Executar apenas no modo de edição
+#if UNITY_EDITOR
+
+	// Entre as rondas
+	private float defaultWaveTimeout = 30f;
+	// Lista com spawners ativos no nível
+	private SpawnPoint[] spawners;
+
+	/// <summary>
+	/// Na atualização do editor.
+	/// </summary>
+	public void Update()
+	{
+		spawners = FindObjectsOfType<SpawnPoint>();
+		int wavesCount = 0;
+		// Obtém o número máximo de rondas dos geradores
+		foreach (SpawnPoint spawner in spawners)
+		{
+			if (spawner.waves.Count > wavesCount)
+			{
+				wavesCount = spawner.waves.Count;
+			}
+		}
+		// Exibe uma lista real com tempos limite das rondas
+		if (wavesTimeouts.Count < wavesCount)
+		{
+			int i;
+			for (i = wavesTimeouts.Count; i < wavesCount; ++i)
+			{
+				wavesTimeouts.Add (defaultWaveTimeout);
+			}
+		}
+		else if (wavesTimeouts.Count > wavesCount)
+		{
+			wavesTimeouts.RemoveRange (wavesCount, wavesTimeouts.Count - wavesCount);
+		}
+	}
+
+#endif
+}
